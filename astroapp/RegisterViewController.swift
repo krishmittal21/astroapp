624d3955f302a849aa96fd12605bded73e49a80e
Krish Mittal <krishmittal212005@gmail.com>
2024-08-08 18:25:31 +0530

    sign up with email

--- astroapp/RegisterViewController.swift
+++ astroapp/RegisterViewController.swift
@@ -18,10 +18,33 @@ class RegisterViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        errorLabel.text = ""
     }
     
     @IBAction func signUpTapped(_ sender: Any) {
+        guard let name = nameTextField.text,
+              let email = emailTextField.text,
+              let password = passwordTextField.text,
+              let confirmPassword = confirmPasswordTextField.text else {
+            errorLabel.text = "Please fill in all fields."
+            return
+        }
+        
+        AuthenticationManager.shared.name = name
+        AuthenticationManager.shared.email = email
+        AuthenticationManager.shared.password = password
+        AuthenticationManager.shared.confirmPassword = confirmPassword
+        
+        AuthenticationManager.shared.signUpWithEmailPassword { [weak self] success, error in
+            guard let self = self else { return }
+            if success {
+                print("Sign-Up successful!")
+            } else if let error = error {
+                self.errorLabel.text = "Sign-Up failed: \(error.localizedDescription)"
+            } else {
+                self.errorLabel.text = AuthenticationManager.shared.errorMessage
+            }
+        }
     }
 }
+
