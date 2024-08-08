b2d888fbaa341520140c4aa4cea6d716f56a8893
Krish Mittal <krishmittal212005@gmail.com>
2024-08-08 14:18:12 +0530

    custom tabbar setup

---
+++ astroapp/TabbarController.swift
@@ -0,0 +1,43 @@
+//
+//  TabbarController.swift
+//  astroapp
+//
+//  Created by Krish Mittal on 08/08/24.
+//
+
+import UIKit
+
+class TabbarController: UITabBarController {
+    
+    var upperLineView: UIView!
+    
+    let spacing: CGFloat = 12
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.delegate = self
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2){
+            self.addTabbarIndicatorView(index: 0, isFirstTime: true)
+        }
+    }
+    
+    ///Add tabbar item indicator uper line
+    func addTabbarIndicatorView(index: Int, isFirstTime: Bool = false){
+        guard let tabView = tabBar.items?[index].value(forKey: "view") as? UIView else {
+            return
+        }
+        if !isFirstTime{
+            upperLineView.removeFromSuperview()
+        }
+        upperLineView = UIView(frame: CGRect(x: tabView.frame.minX + spacing, y: tabView.frame.minY + 0.1, width: tabView.frame.size.width - spacing * 2, height: 4))
+        upperLineView.backgroundColor = UIColor.richPink1
+        tabBar.addSubview(upperLineView)
+    }
+
+}
+
+extension TabbarController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        addTabbarIndicatorView(index: self.selectedIndex)
+    }
+}
