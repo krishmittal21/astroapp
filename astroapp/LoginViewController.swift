152390433546ec456707a05a89d1befd68ff4518
Krish Mittal <krishmittal212005@gmail.com>
2024-08-08 18:28:26 +0530

    sign in with email

--- astroapp/LoginViewController.swift
+++ astroapp/LoginViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var logInButton: UIButton!
@@ -16,11 +16,30 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
     @IBAction func logInTapped(_ sender: Any) {
+        guard let email = emailTextField.text,
+              let password = passwordTextField.text else {
+            errorLabel.text = "Please enter your email and password."
+            return
+        }
+        
+        AuthenticationManager.shared.email = email
+        AuthenticationManager.shared.password = password
+        
+        AuthenticationManager.shared.signInWithEmailPassword { [weak self] success, error in
+            guard let self = self else { return }
+            if success {
+                print("Log-In successful!")
+            } else if let error = error {
+                self.errorLabel.text = "Log-In failed: \(error.localizedDescription)"
+            } else {
+                self.errorLabel.text = AuthenticationManager.shared.errorMessage
+            }
+        }
     }
-
+    
 }
