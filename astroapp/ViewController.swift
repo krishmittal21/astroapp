2f52996c36d76663fe9ea6900815ca6411ecb282
Krish Mittal <krishmittal212005@gmail.com>
2024-08-08 18:17:34 +0530

    storyboard variables in custom classes

--- astroapp/ViewController.swift
+++ astroapp/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var googleSignInButton: UIButton!
+    @IBOutlet weak var appleSignInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func googleButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func appleButtonTapped(_ sender: Any) {
+    }
 }
 
